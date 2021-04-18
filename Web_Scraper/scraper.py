8cdea73a15aa896acdd56f72d6f7be925180f0e8
Darius <Pers2140@fredonia.edu>
2021-04-18 00:07:17 -0400

    JSON includes img_list & gym_info ( website & or address )

--- Web_Scraper/scraper.py
+++ Web_Scraper/scraper.py
@@ -11,7 +11,6 @@ gym_data = {}
 # Returns a list of scraped gyms within div climbing_gyms
 for gym in soup.find_all(class_='text-truncate'):
 
-
     """ scrape each link and follow each link in gym index homepage
         then store results in a dictionary with name of gym as key and link
         and images as values
@@ -22,13 +21,14 @@ for gym in soup.find_all(class_='text-truncate'):
     
     # takes request and parse with lXML
     gym_list_name = gym.find('a').text
-    #print (gym_list_name)
+    gym_data[gym_list_name] = {}
+    print ("\n Getting info on ... " + gym_list_name + "\n")
     gym_list = []
     gym_page = bs(page_r,'lxml')
 
     # for each gym page search for img tag
     for img in gym_page.find_all(class_ ='lazy'):
-        # getting img data src
+        # getting img data src based on class smallMed
         if 'smallMed' in (img)['data-original']:
             #storing img links in gym_list
             gym_list.append((img)['data-original'])
@@ -36,10 +36,24 @@ for gym in soup.find_all(class_='text-truncate'):
 
     # takes imgs and add to gym_data
     new_gym_list = [x for x in gym_list]
-    gym_data[gym_list_name] = new_gym_list
+    gym_data[gym_list_name]['img_list'] = new_gym_list
+
+    # for each gym page search for gym-info class
+    for gym_info in gym_page.find_all(class_= "gym-info"):
+
+        gym_data[gym_list_name]['gym_info'] = [str(a.text) for a in gym_info.find_all('a')]
+        
+        
+        
+
+
+
+
+
+
 
 # from python object to json then write file
-print (gym_data)
+
 with open('data.json', 'w') as outfile:
     json.dump(gym_data, outfile,indent=4)
 
