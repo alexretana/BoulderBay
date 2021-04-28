d94ab854b0aea0a4196251fe66ab5c589fcdae0b
Dboi <Pers2140@fredonia.edu>
2021-04-27 21:32:44 -0400

    Now displays markers matching user input

--- reactfrend/src/Components/Form.js
+++ reactfrend/src/Components/Form.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Result from './Result'
 import GMap from './GMap';
+import Infobar from './InfoBar'
 import data from '../data.json'
 
-function UserForm({results}) {
+function UserForm() {
     // const [eventData, setEventData] = useState([])
     // const [loading, setLoading] = useState(false)
 
@@ -21,28 +22,64 @@ function UserForm({results}) {
     //     fetchEvents()
     //     console.log(eventData)
     // },[])
+
+    const panel_style = {
+        // position:"fixed",
+        position: "relative",
+        float: "left",
+        "z-index": "3000",
+        height: "100vh",
+        overflow: "scroll",
+        background: "rgba(29, 53, 87, 0.9)",
+        "border-radius": 0,
+        color: "white",
+        padding: "25px"
+    }
+    let avg_center = {
+        lat: 38.54555438314078,
+        lng: -97.9853579502318
+    }
+    const search_results = {}
+    const [search, setSearch] = useState('')
+    const [filtered_data, setfiltered_data] = useState({})
     
+      
+    // When search value changes check for matches
+    useEffect(() => {
+        // return filtered out data Object to plug into map and infobox
+        Object.keys(data).map(key => {
+            // if state equals to search add to filtered data
+            if (search == data[key]['state']) {
+                search_results[key] = data[key]
+            }
+            else {
+                console.log('nothing to add')
+            }
+        })
+        setfiltered_data(search_results)
+    }, [search])
+
     return (
         <>
-            <div class="container-fluid " >
-            <br></br>
-                    <br></br>
-           
-                    <input type="text" class="w-50 mx-auto text-center form-control" placeholder="Search Here" id="search" />
-                    <br></br>
-                <p class="m-3">Results: {results.length}</p>
-                    
-          
-            <br></br><br></br>
-            <GMap eventData={data}/>
-            <br></br><br></br>
-            <div class="row " >
-            {results.map( result => {
-                        return (
-                            <Result result={result}/>
-                        )
-                    })}
-            </div>
+            <div class="container-fluid" >
+                <div class="row" >
+
+                    <div class="col-sm-12 col-md-3 " style={panel_style}>
+                        <input className="form-control" type="text" onChange={(e) => setSearch(e.target.value)}  placeholder="Search"></input>
+                        {
+                        
+                            Object.keys(filtered_data).map(key => {
+                                return (
+                                    <Infobar gym_data={filtered_data[key]} />
+                                )
+                            })
+                        
+                        }
+                    </div>
+                    <div class="" >
+                        <GMap eventData={filtered_data} center={avg_center}/>
+                    </div>
+                </div>
             </div>
         </>
     )
