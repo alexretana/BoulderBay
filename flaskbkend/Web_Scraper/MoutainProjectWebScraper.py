7bbb3766c5cdcc04d365e47e39db7d1e6c270afd
alexretana <alex.retana@live.com>
2021-05-11 16:38:52 -0400

    Cleans format

--- flaskbkend/Web_Scraper/MoutainProjectWebScraper.py
+++ flaskbkend/Web_Scraper/MoutainProjectWebScraper.py
@@ -2,12 +2,15 @@ from bs4 import BeautifulSoup as bs
 import requests
 import json
 from time import sleep
-import logging 
+import logging
 from config import listOfStates
 
-logging.basicConfig(filename="errors.log", level=logging.WARNING, format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(filename="errors.log", level=logging.WARNING,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 # scrapes info and images from RC Gyms per State
+
+
 def scrapeStateFromMoutainProject(state):
     # get source html file with http request
     state_url = 'https://www.mountainproject.com/gyms/' + state
@@ -16,6 +19,7 @@ def scrapeStateFromMoutainProject(state):
     # takes request and parses with lxml
     soup = bs(req, 'lxml')
     gym_data = {}
+    
     # Returns a list of scraped gyms within div climbing_gyms
     for gym in soup.find_all(class_='text-truncate'):
 
@@ -29,18 +33,24 @@ def scrapeStateFromMoutainProject(state):
 
         # takes request and parse with lXML
         gym_list_name = gym.find('a').text
-        
+
         # print(("getting info on %s") %(gym_list_name))
         if '…' in gym_list_name:
             try:
-                logging.warning('\n*** Dots ... in %s***\n' %(gym_list_name))
+                logging.warning('\n*** Dots ... in %s***\n' % (gym_list_name))
+                
                 # Follow gym_list_name and get full name instead of ...X
-                for  line in (bs(requests.get(link).text,'lxml').find_all(id='climbing-gyms')):
-                    print (str(line.find('h1').text.strip().decode('utf-8')))
-                    cleaned_title = str(line.find('h1').text.strip().decode('utf-8'))
+                for line in (bs(requests.get(link).text, 'lxml').find_all(id='climbing-gyms')):
+                    
+                    print(str(line.find('h1').text.strip().decode('utf-8')))
+                    cleaned_title = str(
+                        line.find('h1').text.strip().decode('utf-8'))
+                    
                     gym_data[cleaned_title] = {}
+                    
                     # adding location name to gym data object
                     gym_data[cleaned_title]['locName'] = cleaned_title
+                    
                     # adding state to gym data object
                     gym_data[cleaned_title]["state"] = state
                     gym_list = []
@@ -48,9 +58,11 @@ def scrapeStateFromMoutainProject(state):
 
                     # for each gym page search for img tag
                     for img in gym_page.find_all(class_='lazy'):
+                        
                         # getting img data src based on class smallMed
                         if 'smallMed' in img['data-original']:
-                            #storing img links in gym_list
+                            
+                            # storing img links in gym_list
                             gym_list.append(img['data-original'])
                             #print  ((img)['data-original'])
 
@@ -60,18 +72,20 @@ def scrapeStateFromMoutainProject(state):
                     for gym_info in gym_page.find_all(class_="gym-info"):
 
                         gym_data[cleaned_title]['gym_info'] = [
-                        a.text for a in gym_info.find_all('a')]
+                            a.text for a in gym_info.find_all('a')]
             except:
 
-                    logging.warning('Some major shit went wrong ... moving on')
-                    pass
+                logging.warning('Some major shit went wrong ... moving on')
+                pass
         else:
             try:
                 print(gym_list_name)
+                
                 gym_data[gym_list_name] = {}
 
                 # adding location name to gym data object
                 gym_data[gym_list_name]['locName'] = gym_list_name
+
                 # adding state to gym data object
                 gym_data[gym_list_name]["state"] = state
 
@@ -80,19 +94,22 @@ def scrapeStateFromMoutainProject(state):
 
                 # for each gym page search for img tag
                 for img in gym_page.find_all(class_='lazy'):
+
+
                     # getting img data src based on class smallMed
                     if 'smallMed' in img['data-original']:
-                        #storing img links in gym_list
+                        # storing img links in gym_list
                         gym_list.append(img['data-original'])
                         #print  ((img)['data-original'])
 
                 # takes imgs and add to gym_data
                 gym_data[gym_list_name]['img_list'] = gym_list
+
                 # for each gym page search for gym-info class
                 for gym_info in gym_page.find_all(class_="gym-info"):
 
                     gym_data[gym_list_name]['gym_info'] = [
-                    a.text for a in gym_info.find_all('a')]
+                        a.text for a in gym_info.find_all('a')]
             except:
 
                 logging.warning('Some major shit went wrong ... moving on')
@@ -104,22 +121,23 @@ def scrapeStateFromMoutainProject(state):
 
     return gym_data
 
+
 if __name__ == "__main__":
-    
+
     # initiallizes dict to dump data into
     gym_data = {}
     # success count
     count = 0
     # test list override
-    
+
     # loop through each state to scrap
     for state in listOfStates:
-        print ("\n *** Getting goodies from ... %s ***\n"%(state))
+        print("\n *** Getting goodies from ... %s ***\n" % (state))
         state = state.replace(" ", "-")
-        
+
         try:
             scrapped_gym_data = scrapeStateFromMoutainProject(state)
-            count+=1
+            count += 1
         except:
             pass
 
@@ -133,5 +151,5 @@ if __name__ == "__main__":
 
         # # write to json for every state
         with open('data.json', 'w') as outfile:
-            json.dump(gym_data, outfile,indent=4)
-            print ("All data from %s state/s now successfully written to file!" %(count))
+            json.dump(gym_data, outfile, indent=4)
+            print("All data from %s state/s now successfully written to file!" % (count))
