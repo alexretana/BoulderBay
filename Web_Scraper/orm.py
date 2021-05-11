ce81cef62188914be3078f7a8e83b578462cd8d9
alexretana <alex.retana@live.com>
2021-05-11 11:08:16 -0400

    Addes classes to orm

--- Web_Scraper/orm.py
+++ Web_Scraper/orm.py
@@ -12,12 +12,53 @@ endpoint = DB_ENDPOINT
 dialect = f"mysql+pymysql://{user}:{pwd}@{endpoint}/boulderinggyms"
 
 #create engind and bind to session
-## Remove echo=True when done debugging
-engine = create_engine(dialect, echo = True, future=True)
 Base = declarative_base()
 metadata = Base.metadata
-session = Session
+
+#define table connected to clases
+gyms_table = Table(
+    "gyms",
+    metadata,
+    Column('gymID', BIGINT(unsigned=True), primary_key=True, autoincrement=True),
+    Column('gymName', VARCHAR(80), nullable=False),
+    Column('gymNameFromGoogle', VARCHAR(80)),
+    Column('gymAddress', VARCHAR(100)),
+    Column('locLatitude', DECIMAL(8,6)),
+    Column('locLongitude', DECIMAL(8,6)),
+    Column('ratingFromMP', DECIMAL(8,6)),
+    Column('googlePlaceID', VARCHAR(255)),
+    Column('lastUpdated', TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+)
+
+photos_table = Table(
+    "photos",
+    metadata,
+    Column('photoID', BIGINT(unsigned=True), primary_key=True, autoincrement=True),
+    Column('gymID', BIGINT(unsigned=True), ForeignKey('gyms.gymID'), nullable=False),
+    Column('photoGoogleReference', VARCHAR(255)),
+    Column('photoURL', VARCHAR(160)),
+    Column('lastUpdated', TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+)
+
+class Gym(Base):
+    __table__ = gyms_table
+    
+    photo = relationship("Photo", back_populates="gym")
+    
+    def __repr__(self):
+        return f"Gym({self.gymID!r}, {self.gymName!r}, {self.gymNameFromGoogle!r}, {self.gymAddress!r},  {self.locLatitude!r}, {self.locLongitude}, {self.ratingFromMP!r}, {self.googlePlaceID!r}, {self.lastUpdated!r})"
+    
+class Photo(Base):
+    __table__ = photos_table
+    
+    gym = relationship("Gym", back_populates="photo")
+    
+    def __repr__(self):
+        return f"Photo({self.photoID!r}, {self.gymID!r}, {self.photoGoogleReference!r}, {self.photoURL!r}, {self.lastUpdated!r})"
+
+
 
 #if orm is run, it builds tables for db
 if __name__== '__main__':
-    metadata = Base.metadata
+    engine = create_engine(dialect, echo = True, future=True)
+    metadata.create_all(engine)
